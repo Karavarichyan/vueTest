48270a9bf1bc862b2dc2268a6f247b555f1209cf
Narek <narek@mail.com>
2024-01-29 18:43:37 +0400

    chene axios

--- src/views/login/NewEmailView.vue
+++ src/views/login/NewEmailView.vue
@@ -1,56 +1,69 @@
 <template>
-    <div class="text-xl w-1/2 ml-28  leading-8 font-medium text-indigo-800 w-350 h-370; py-8">
-        <h2>Create Post</h2>
-        <hr />
-
-        <div  v-if="isSuccess">
-            Post Created Successfully
+    <div class="text-xl w-1/2 ml-28 leading-8 font-medium text-indigo-800 w-350 h-370 py-8">
+      <div>
+        <p>Email input</p>
+        {{ email }}
+      </div>
+      <div>
+        <p>ssss</p>
+        {{ value }}
+      </div>
+      <form @submit.prevent="onCreatePost">
+        <div>
+          <input type="email" v-model="email" placeholder="Enter email" class="outline-0 max-w-sm my-6 border-b-2 border-color: rgb(203 213 225); w-full" />
         </div>
-        <form @submit.prevent="onCreatePost">
-            <div class="form-group">
-                <label>Title</label>
-                <input type="email" class="outline-0 max-w-sm my-6 border-b-2 border-color: rgb(203 213 225); w-full" v-model="title" />
-            </div>
-            <!-- <div class="form-group">
-                <label>Description</label>
-                <textarea
-                    class="form-control"
-                    v-model="description"
-                ></textarea>
-            </div> -->
-
-            <div class="mt-3">
-                <button type="submit" class="btn btn-primary">
-                    Create Post
-                </button>
-            </div>
-        </form>
+        <div class="mt-3">
+          <button type="submit" class="btn btn-primary" :disabled="!isValidEmail">
+            Create Post
+          </button>
+        </div>
+      </form>
     </div>
-</template>
-
-<script>
-import axios from 'axios';
-export default {
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
     data() {
-        return {
-            title: '',
-            // description: '',
-            isSuccess: false,
-        };
+      return {
+        email: '',
+       
+        isSuccess: false,
+      };
     },
+  
+    computed: {
+      isValidEmail() {
+        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return this.email && emailRegex.test(this.email);
+      },
+    },
+  
     methods: {
-        onCreatePost() {
-            axios
-                .post('https://jsonplaceholder.typicode.com/posts', {
-                    title: this.title,
-                    // description: this.description,
-                })
-                .then((response) => {
-                    this.isSuccess = true;
-                    console.log(response);
-                    this.$emit('postcreated');
-                });
-        },
+      onCreatePost() {
+       
+        if (this.isValidEmail) {
+          const enteredEmail = this.email;
+  
+          axios
+            .get('https://jsonplaceholder.typicode.com/users', {
+              email: enteredEmail,
+            })
+            .then((response) => {
+              this.isSuccess = true;
+              console.log(response);
+              console.log(enteredEmail)
+             
+            //   this.$emit('postcreated');
+            });
+        } else {
+          console.log('Invalid email. Please enter a valid email address.');
+         
+        }
+      },
     },
-};
-</script>
+  };
+  </script>
+  
