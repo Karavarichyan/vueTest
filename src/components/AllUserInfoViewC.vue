f5e273375da02761c03df566a7b8a8141a420557
Narek <narek@mail.com>
2024-02-06 15:22:13 +0400

    finsh cheng tab

--- src/components/AllUserInfoViewC.vue
+++ src/components/AllUserInfoViewC.vue
@@ -1,24 +1,29 @@
-<template>
+
+  <template>
     <div class="container mx-auto mt-8">
-      <h1 class="text-4xl font-bold mb-8 text-center text-gray-800">Posts Information</h1>
+      <h1 class="text-4xl font-bold mb-8 text-center text-gray-800">Info in Posts</h1>
       <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <li v-for="post in posts" :key="post.id" class="mb-8 p-6 bg-white border border-gray-300 rounded-md shadow-md hover:shadow-lg transition">
+        <li v-for="post in Enyposts" :key="post.id" class="mb-8 p-6 bg-white border border-gray-300 rounded-md shadow-md hover:shadow-lg transition">
           <h2 class="text-xl font-semibold mb-4 text-gray-900">{{ post.title }}</h2>
           <p class="text-gray-700">{{ post.body }}</p>
         </li>
       </ul>
     </div>
   </template>
-  
   <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, computed } from 'vue';
   import axios from 'axios';
-    const posts = ref([]);
-  onMounted(async () => {
+  const posts = ref([]);
+  const MyPosts = async () => {
     try {
-      posts.value = (await axios.get('https://jsonplaceholder.typicode.com/posts')).data;
+      return (await axios.get('https://jsonplaceholder.typicode.com/posts')).data;
     } catch (error) {
-      console.error('Error fetching posts:', error);
+      console.error('error:', error);
+      return [];
     }
+  };
+  const Enyposts = computed(() => posts.value);
+  MyPosts().then((data) => {
+    posts.value = data;
   });
   </script>
